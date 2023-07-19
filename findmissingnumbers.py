dedeec535d27e00257233f1f62683b517eb63565
Asa Paparo <asapaparo@gmail.com>
2023-07-19 09:09:32 -0400

    aaa

--- findmissingnumbers.py
+++ findmissingnumbers.py
@@ -59,7 +59,7 @@ class Solution:
         missing = []
 
         last = numbers[0]
-        for i in range(1, numbers):
+        for i in range(1, len(numbers)):
             if numbers[i] != numbers[i-1]:
                 for i in range(numbers[i-1], numbers[i]):
                     missing.append(i)
