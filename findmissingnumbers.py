3f9145a545f79aa653e6e97708ebd8725b608acc
Asa Paparo <asapaparo@gmail.com>
2023-07-19 09:11:02 -0400

    int?

--- findmissingnumbers.py
+++ findmissingnumbers.py
@@ -61,7 +61,7 @@ class Solution:
         last = numbers[0]
         for i in range(1, len(numbers)):
             if numbers[i] != numbers[i-1]:
-                for i in range(numbers[i-1], numbers[i]):
+                for i in range(int(numbers[i-1]), int(numbers[i])):
                     missing.append(i)
         
         return missing
